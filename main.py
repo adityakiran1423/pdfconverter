6aebcc35059aa241aa1e7d3c69043476adb556d7
adityakiran1423 <adi.kiran@gmail.com>
2026-01-01 14:12:51 +0530

    feat(main): adding check to check if file name entered is a pdf or not

--- main.py
+++ main.py
@@ -1,7 +1,20 @@
-import argparse
+import sys
+
 
 def main():
-    ...
+    pdf_file_path = sys.argv[1]
+    conversion_type = sys.argv[2]
+
+    if pdf_file_path[-4:] != ".pdf":
+        print("the file you have entered is not a pdf")
+        sys.exit()
+    else:
+        # check if file actually exists
+        # if pdf should be converted to excel then use tabula-py
+        # if pdf should be converted to word then use dfminer.six
+        print(f"pdf file path is : {pdf_file_path}")
+        print(f"conversion type is : {conversion_type}")
+
 
 if __name__ == "__main__":
     main()
